26d73741ff401c49df456f991d020d9d61cdc793
Seyf02 <malikcansu60@gmail.com>
2026-05-14 10:14:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-
+const CACHE='hantatakip-v3';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.json'])));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const h=new URL(e.request.url).hostname;if(['rss2json.com','hantacount.com','cartocdn.com','unpkg.com'].some(d=>h.includes(d))){e.respondWith(fetch(e.request).catch(()=>new Response('',{status:503})));return;}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)));});
